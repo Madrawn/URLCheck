926970970f2d52759b9607ea474df522061efe36
Madrawn <Madrawn@Madrawn-PC.fritz.box>
2017-06-29 14:13:29 +0200

    Catching error with wrong separator

--- src/main/java/de/daniel/dengler/URLChecker/Controller.java
+++ src/main/java/de/daniel/dengler/URLChecker/Controller.java
@@ -14,7 +14,7 @@ import javax.swing.JFileChooser;
 import javax.swing.JProgressBar;
 import javax.swing.JTextArea;
 
-public class Controller implements Runnable{
+public class Controller implements Runnable {
 
 	File selectedFile;
 	private MainWindow mainWindow;
@@ -32,8 +32,8 @@ public class Controller implements Runnable{
 
 	public void setFile(File selectedFile) {
 		this.selectedFile = selectedFile;
-		
-		//This is the first row with the titles of each column
+
+		// This is the first row with the titles of each column
 		title.add("Start URL");
 		title.add("Matches");
 		title.add("End URL");
@@ -43,11 +43,11 @@ public class Controller implements Runnable{
 	}
 
 	public void checkTheFile() {
-		//disable the check button again
+		// disable the check button again
 		mainWindow.setCheckButtonEnabled(false);
-		
+
 		JTextArea jta = mainWindow.getTextArea();
-		//get the lines from the file
+		// get the lines from the file
 		List<String> lines = Helper.readFile(selectedFile.getAbsolutePath());
 		if (!lines.isEmpty()) {
 			String[][] table = CsvWrapper.readCsv(lines);
@@ -57,53 +57,73 @@ public class Controller implements Runnable{
 			JProgressBar pb = mainWindow.getProgressBar();
 			pb.setMaximum(lines.size());
 			
-			//We assume the file has titles in the first row. copy them to the right of the new table titles
+			//Only one column might be an error notifiy
+			if (table[1].length == 1) {
+				jta.append("\n \n Die CSV hat nur eine einzige Spalte.");
+			}
+			
+			//This is definitly an error.
+			if (table[1].length < mainWindow.getRelevantColumn() +1) {
+				jta.append("\n \n Sie haben die "
+						+ (mainWindow.getRelevantColumn() +1)
+						+ ". Spalte gewählt, die Tabelle"
+						+ " hat jedoch nur "
+						+ table[1].length
+						+ " Spalte\\n\n"
+						+ "Vielleich wurde die CSV mit dem falschen Trenner exportiert?");
+				return;
+
+			}
+
+			// We assume the file has titles in the first row. copy them to the
+			// right of the new table titles
 			String[] originalTitles = table[0];
 			for (int i = 0; i < originalTitles.length; i++) {
 				title.add(originalTitles[i]);
 			}
-			
-			
-			//begin checking
+
+			// begin checking
 			jta.append("\n \n Checking URLs");
-			//skip titles, start at 1
+			// skip titles, start at 1
 			for (int i = 1; i < table.length; i++) {
-				//check every url and generate the export table
+				// check every url and generate the export table
 				String e = table[i][mainWindow.getRelevantColumn()];
 				UrlChecker urlChecker = null;
-				
-				//if we already checked then we don't need to again saving a lot of time
+
+				// if we already checked then we don't need to again saving a
+				// lot of time
 				boolean alreadyChecked = false;
-				for(UrlChecker u : alreadyCheckedUrls){
-					if(u.getStartURL().equals(e)){
+				for (UrlChecker u : alreadyCheckedUrls) {
+					if (u.getStartURL().equals(e)) {
 						alreadyChecked = true;
 						urlChecker = u;
 					}
 				}
-				
+
 				try {
-					if(!alreadyChecked){
+					if (!alreadyChecked) {
 						urlChecker = new UrlChecker(new URL(e));
 					}
-					List<String> checkedLine = addLine(urlChecker.getStartURL(),
-							urlChecker.getMatches(), urlChecker.getNewUrl(),
-							urlChecker.getGuessedCorrectUrl(),table[i]);
+					List<String> checkedLine = addLine(
+							urlChecker.getStartURL(), urlChecker.getMatches(),
+							urlChecker.getNewUrl(),
+							urlChecker.getGuessedCorrectUrl(), table[i]);
 					workingTable.add(checkedLine);
 					alreadyCheckedUrls.add(urlChecker);
 					jta.append("\n" + e);
-					pb.setValue(i+1);
+					pb.setValue(i + 1);
 				} catch (MalformedURLException e1) {
-					jta.append("\n"+e+" ist keine korrekte URL");
+					jta.append("\n" + e + " ist keine korrekte URL");
 					e1.printStackTrace();
 				} catch (IOException e1) {
-					jta.append("\n"+e+" <-> URL <-> IOException");
-					
+					jta.append("\n" + e + " <-> URL <-> IOException");
+
 					e1.printStackTrace();
 				}
 
 			}
-			//we have checked all the URLs enable the export button
-			
+			// we have checked all the URLs enable the export button
+
 			jta.append("\n \n FINISHED! Sie können das Ergebniss jetzt exportieren.");
 			mainWindow.setExportButtonEnabled(true);
 
@@ -114,38 +134,37 @@ public class Controller implements Runnable{
 	}
 
 	public void export() {
-		
-		
+
 		JFileChooser jfc = new JFileChooser();
 		int result = jfc.showSaveDialog(mainWindow.getJPanel());
-		if(result == JFileChooser.APPROVE_OPTION){
-			
-			
-			String path = jfc.getSelectedFile().getAbsolutePath() +".csv";
-			
-			//Turn the list into a array
+		if (result == JFileChooser.APPROVE_OPTION) {
+
+			String path = jfc.getSelectedFile().getAbsolutePath() + ".csv";
+
+			// Turn the list into a array
 			String[][] table = new String[workingTable.size()][];
 			int index = 0;
-			for(List<String> l : workingTable){
+			for (List<String> l : workingTable) {
 				table[index] = l.toArray(new String[0]);
 				index++;
 			}
-			//write the file.
+			// write the file.
 			try {
-				
-				CsvWrapper.writeCSV(Paths.get(path), table);
-				mainWindow.getTextArea().append("\n-------- Export fertig! --------");
 
-				//everything went fine we can disable the export button
+				CsvWrapper.writeCSV(Paths.get(path), table);
+				mainWindow.getTextArea().append(
+						"\n-------- Export fertig! --------");
+
+				// everything went fine we can disable the export button
 				mainWindow.setExportButtonEnabled(false);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
 
-	//helper method for generating a line as a list
+	// helper method for generating a line as a list
 	private List<String> addLine(String startURL, boolean matches,
 			String newUrl, String guessedCorrectUrl, String[] rest) {
 		List<String> line = new LinkedList<String>();
@@ -158,7 +177,5 @@ public class Controller implements Runnable{
 		}
 		return line;
 	}
-
-
 
 }
